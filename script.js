3ed61e63cf8b62f85a85f2d32cf71b1d44389eaa
Luigi <luigi.n.battista@gmail.com>
2019-02-26 10:52:53 -0700

    Sorted by movie title

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-import { films } from './films.js'
+import {
+  films
+} from './films.js'
 
-console.log(films)
+const intro = document.querySelector('#welcome-section')
+
+films.sort ((a,b) => (a.episode_id > b.episode_id) ? 1 : -1)
+
+films.forEach((film) => {
+  let div = document.createElement("div")
+  div.className = "card"
+  intro.appendChild(div)
+  
+  let titleElement = document.createElement('h1')
+  intro.appendChild(titleElement)
+  titleElement.textContent = film.title
+
+  let crawlElement = document.createElement('p')
+  intro.appendChild(crawlElement)
+  crawlElement.textContent = film.opening_crawl
+})
