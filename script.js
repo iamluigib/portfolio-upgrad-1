b74dacb29d1ae77d7e9c28585f852dd49585896b
Luigi <hello@iamluigib.com>
2019-02-28 10:51:27 -0700

    Added movies title and crawl

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-import {
-  films
-} from './films.js'
+import { films } from './films.js'
+import { people } from './people.js'
 
 const intro = document.querySelector('#welcome-section')
 
 films.sort ((a,b) => (a.episode_id > b.episode_id) ? 1 : -1)
 
 films.forEach((film) => {
-  let div = document.createElement("div")
-  div.className = "card"
-  intro.appendChild(div)
+  let tile = document.createElement("div")
   
   let titleElement = document.createElement('h1')
-  intro.appendChild(titleElement)
+  tile.appendChild(titleElement)
   titleElement.textContent = film.title
 
   let crawlElement = document.createElement('p')
-  intro.appendChild(crawlElement)
+  tile.appendChild(crawlElement)
   crawlElement.textContent = film.opening_crawl
+
+  intro.appendChild(tile)
 })
+
