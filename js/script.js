60b9cf4be83a5beddd4acc63197df23f00cad4de
Luigi <hello@iamluigib.com>
2019-03-03 01:40:35 -0700

    Cleaned up CSS

--- js/script.js
+++ js/script.js
@@ -7,15 +7,32 @@ const intro = document.querySelector('#welcome-section')
 films.sort((a, b) => (a.episode_id > b.episode_id) ? 1 : -1)
 
 films.forEach((film) => {
-  let tile = document.createElement("div")
+  let container = document.createElement('div')
+  container.className = "container"
+  intro.appendChild(container)
 
-  let titleElement = document.createElement('h1')
-  tile.appendChild(titleElement)
+  let row = document.createElement('div')
+  row.className = 'row'
+  container.appendChild(row)
+
+  let column = document.createElement('div')
+  column.className = 'col s12'
+  row.appendChild(column)
+
+  let card = document.createElement("div")
+  card.className = "card"
+  column.appendChild(card)
+
+  let cardContent = document.createElement('div')
+  cardContent.className = 'card-content'
+  card.appendChild(cardContent)
+
+  let titleElement = document.createElement('span')
+  titleElement.className = "card-title"
+  cardContent.appendChild(titleElement)
   titleElement.textContent = film.title
 
   let crawlElement = document.createElement('p')
-  tile.appendChild(crawlElement)
+  cardContent.appendChild(crawlElement)
   crawlElement.textContent = film.opening_crawl
-
-  intro.appendChild(tile)
 })
