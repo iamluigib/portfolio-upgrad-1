b832a148b08910961e982abf0b1e7a11da2fa059
Luigi <hello@iamluigib.com>
2019-03-05 10:56:55 -0700

    Added SWPeople

--- js/swpeople.js
+++ js/swpeople.js
@@ -6,4 +6,15 @@ const women = people.filter(person => person.gender === "female")
 
 const other = people.filter(person => (person.gender === "n/a") || (person.gender === "none") || (person.gender === "hermaphrodite"))
 
-console.log(other)
+// console.log(men, women, other)
+
+men.forEach(man => {
+  let manDiv = document.createElement('div')
+  manDiv.className = 'card-panel'
+
+  let manName = document.createElement('p')
+  manName.textContent = man.name;
+  manDiv.appendChild('manName')
+
+  document.appendChild('manDiv')
+})
