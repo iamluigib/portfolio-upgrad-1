7cddc86e1656e8f68fc5cd9f763c178f0260c7ae
Luigi <hello@iamluigib.com>
2019-04-18 19:00:55 -0600

    Added pokemon card setup
    And images.

--- js/pokecards.js
+++ js/pokecards.js
@@ -2,39 +2,82 @@ import {
   pokemon
 } from '../data/pokemon.js'
 
-pokemon.forEach((singleMon) => {
-  fetch(singleMon.url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      createPokeCard(myJson);
-    });
-})
+class Pokemon {
+  constructor(id) {
+    this.id = id
+  }
+}
 
-const cardWrapper = document.querySelector('.container')
+const mainContainer = document.querySelector('.row')
 
-function createPokeCard(pokedata) {
+function createPokeCard(pokeData) {
+  let column = document.createElement('div')
+  column.className = "col s12 m6 l4 xl3"
   let card = document.createElement('div')
-  card.className = 'card'
-  cardWrapper.appendChild(card)
-
+  card.className = 'card center-align hoverable'
   let cardImage = document.createElement('div')
   cardImage.className = 'card-image'
-  card.appendChild(cardImage)
-
-  let imgElement = document.createElement('img')
-  imgElement.src = pokedata.front_default
-  imgElement.alt = pokedata.name
-  cardImage.appendChild(imgElement)
-
-
+  let image = document.createElement('img')
   let cardContent = document.createElement('div')
   cardContent.className = 'card-content'
-  card.appendChild(cardContent)
+  let cardTitle = document.createElement('span')
+  cardTitle.className = 'card-title truncate'
 
-  let name = document.createElement('span')
-  name.className = 'card-title'
-  name.textContent = pokedata.name
-  cardContent.appendChild(name)
+  //let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
+  cardTitle.textContent = pokeData.name
+  if (pokeData.id !== 0) {
+    image.src = `../images/${pokeData.imageID}${pokeData.name}.png`
+  } else {
+    image.src = `../images/pokeball.png`
+  }
+
+  cardContent.appendChild(cardTitle)
+  cardImage.appendChild(image)
+  card.appendChild(cardImage)
+  card.appendChild(cardContent)
+  column.appendChild(card)
+  mainContainer.appendChild(column)
 }
+
+pokemon.forEach(singleMon => {
+  fetch(singleMon.url)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (myJson) {
+
+      createPokeCard(matchIdToImage(myJson))
+    })
+})
+
+function matchIdToImage(aPokemon) {
+  if (aPokemon.id < 10) {
+    aPokemon.imageID = "00" + aPokemon.id
+  }
+  if (aPokemon.id > 9 && aPokemon.id < 100) {
+    aPokemon.imageID = "0" + aPokemon.id
+  }
+  if (aPokemon.id > 99) {
+    aPokemon.imageID = aPokemon.id
+  }
+  aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1)
+  return aPokemon
+}
+
+function fetchSinglePokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (retrievedPokemon) {
+      createPokeCard(matchIdToImage(retrievedPokemon))
+    })
+}
+
+
+const newPokemonButton = document.querySelector('button')
+
+newPokemonButton.addEventListener('click', function () {
+  let pokemonID = prompt('Enter an ID of an existing pokemon:')
+  fetchSinglePokemon(pokemonID)
+});
